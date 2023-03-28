55b3ab9ccc1c8f88f2f39bd524d6190a0f37c31c
Chalini11 <chalini.pasupathy@m2pfintech.com>
2023-03-28 20:05:44 +0530

    Update: Program can detect whether the tree is mango tree

--- src/Garden.java
+++ src/Garden.java
@@ -1,6 +1,28 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Garden {
     public static void main(String[] args)
     {
-        System.out.println("Dora's Garden");
+        System.out.println(" Welcome to Dora's Garden");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter -> No of Rows and Columns and finally the Tree number");
+        int rowsInGarden = scan.nextInt();
+        int columnsInGarden = scan.nextInt();
+        int treeNumber = scan.nextInt();
+
+        System.out.println(mangoChecker(rowsInGarden,columnsInGarden,treeNumber));
+
+    }
+
+    private static String mangoChecker(int rowsInGarden, int columnsInGarden, int treeNumber)
+    {
+        if(treeNumber >=1 && treeNumber <= columnsInGarden)
+            return "yes";
+        else if (treeNumber <= rowsInGarden*columnsInGarden && (treeNumber >= columnsInGarden*(rowsInGarden-1)+1))
+                return "yes";
+        else if(treeNumber%columnsInGarden==1)
+               return "yes";
+        else return "no";
     }
 }
